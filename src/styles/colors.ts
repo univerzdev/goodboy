07c42eddc15cb0aa439a23a35b3e4db8d2214323
univerzdev <martinplavka17@gmail.com>
2026-07-12 14:27:41 +0200

    [app] - ADD: colors definition

---
+++ src/styles/colors.ts
@@ -0,0 +1,40 @@
+export const colors = {
+  primary: "#111827",
+  secondary: "#f3f4f6",
+  tertiary: "#4b5563",
+  quintarny: "#d1d5db",
+  quaternary: "#9ca3af",
+  inverse: "#fafafa",
+
+  action: {
+    primary: {
+      base: "#4f46e5",
+      hover: "#4338ca",
+      active: "#3730a3",
+      loading: "#4f46e5",
+      disabled: "#4f46e5",
+    },
+    secondary: {
+      base: "#f3f4f6",
+      hover: "#e5e7eb",
+      active: "#d1d5db",
+      loading: "#f3f4f6",
+      disabled: "#f3f4f6",
+    },
+    destructive: {
+      base: "#e11d48",
+      hover: "#be123c",
+      active: "#9f1239",
+      loading: "#e11d48",
+      disabled: "#e11d48",
+    },
+  },
+
+  semantic: {
+    success: "#047857",
+    warning: "#b45309",
+    error: "#be123c",
+  },
+
+  background: "#ffffff",
+} as const;
